1ef5d357c10bf6f2a9cb2371e49b22b9be0b03e3
Bilal <bilalbelkebir9@gmail.com>
2024-10-14 13:01:06 +0100

    create the routing for all the component and fetch the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Home from "./Home/Home";
+import About from "./About/About";
+import HomeContent from "./Home/HomeContent";
+import Vans from "./Vans/Vans";
+import { useState , useEffect } from "react";
+import VansDetails from "./Vans/VansDetails"
+import Host from './Host/Host'
+import Dashbord from './Host/Dashbord'
+import Income from './Host/Income'
+import Reviews from './Host/Reviews'
+import HostVans from './Host/HostVans'
+import HostVanDetail from './Host/HostVanDetail'
+import Detail from "./Host/Details";
+import Pricing from "./Host/Pricing";
+import Photos from "./Host/Photos";
 
 function App() {
+  const [data , setData] = useState([])
+  useEffect(() => {
+      async function getData() {
+          const data = await fetch('https://api.jsonbin.io/v3/b/670aa770ad19ca34f8b73e1b')
+          const res = await data.json()
+          setData(res.record)
+      }
+      getData()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeContent />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans data={data} />} />
+          <Route path="vans/:id" element={<VansDetails data={data} />} />
+          <Route path="host" element={<Host />}>
+            <Route index element={<Dashbord />} />
+            <Route path="van" element={<HostVans data={data} />} />
+            <Route path="van/:id" element={<HostVanDetail data={data} />}>
+              <Route index element={<Detail data={'nothing'} />} />
+              <Route path='pricing' element={<Pricing />} />
+              <Route path='photos' element={<Photos />} />
+            </Route>
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
