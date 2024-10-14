8d91cc686f23aef38d68ad98385a2719f439e1e7
Bilal <bilalbelkebir9@gmail.com>
2024-10-14 12:50:33 +0100

    create the home page and style it

---
+++ src/Home/Home.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import "./Home.css"
+
+export default function Home() {
+    return (
+        <>
+            <header>
+                <h2><Link to="/">#VANLIFE</Link></h2>
+                <nav className="home-nav">
+                    <ul>
+                        <Link to="/host">Host</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/vans">Vans</Link>
+                    </ul>
+                </nav>
+            </header>
+            <Outlet />
+            <footer>Ⓒ 2022 #VANLIFE</footer>
+        </>
+    )
+}
