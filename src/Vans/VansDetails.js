a487723b7d7f8124ffc4d2b864c8833df10bf2db
Bilal <bilalbelkebir9@gmail.com>
2024-10-17 21:33:52 +0100

    modify the vans back to back the specific van type selected

--- src/Vans/VansDetails.js
+++ src/Vans/VansDetails.js
@@ -1,14 +1,21 @@
-import { useParams } from 'react-router-dom'
+import { useParams , Link , useLocation} from 'react-router-dom'
 import './VansDetails.css'
 
 export default function VansDetails({data}) {
     const {id} = useParams()
+    const location = useLocation()
+    const searchParam = location.state.type
     const vandetails = data.find(el => el.id === id)
     if (!data) {
         return <h1>loading...</h1>
     }
     return (
         <div className="van-detail-container">
+            <Link
+                to={`..${searchParam ? `?type=${searchParam}` : ""}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>{`Back to ${searchParam ? `${searchParam}` : "all"} vans`}</span></Link>
             <div className="van-detail">
                 <img src={vandetails.imageUrl} alt='van-detail' />
                 <i className={`van-type ${vandetails.type} selected`}>{vandetails.type}</i>
